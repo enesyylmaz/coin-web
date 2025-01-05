df856e985e3f1942ba69194839f01b3522a1c628
enesyylmaz <98919812+enesyylmaz@users.noreply.github.com>
2025-01-05 19:03:43 +0300

    frontend & post req

--- Web Application/Pages/Index.cshtml.cs
+++ Web Application/Pages/Index.cshtml.cs
@@ -1,20 +1,64 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
-namespace Web_Application.Pages
+public class IndexModel : PageModel
 {
-    public class IndexModel : PageModel
-    {
-        private readonly ILogger<IndexModel> _logger;
+    [BindProperty]
+    public IFormFile ImageFile { get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+    [BindProperty]
+    public string CroppedImageData { get; set; }
+
+    public string JsonResponse { get; set; }
+
+    private readonly IHttpClientFactory _clientFactory;
+
+    public IndexModel(IHttpClientFactory clientFactory)
+    {
+        _clientFactory = clientFactory;
+    }
+
+    public void OnGet()
+    {
+    }
+
+    public async Task OnPostAsync()
+    {
+        if (ImageFile != null && ImageFile.Length > 0)
         {
-            _logger = logger;
+            using (var stream = new MemoryStream())
+            {
+                await ImageFile.CopyToAsync(stream);
+                var byteArray = stream.ToArray();
+            }
         }
 
-        public void OnGet()
+        if (!string.IsNullOrEmpty(CroppedImageData))
         {
+            var base64Data = CroppedImageData.Substring(CroppedImageData.IndexOf(",") + 1);
+            var byteArray = Convert.FromBase64String(base64Data);
 
+            using (var client = _clientFactory.CreateClient())
+            {
+                var content = new ByteArrayContent(byteArray);
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+
+                var response = await client.PostAsync("https://torchserve-mobilenetv3.onrender.com/predictions/mv3l_aug", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    JsonResponse = await response.Content.ReadAsStringAsync();
+                }
+                else
+                {
+                    JsonResponse = "Error processing the cropped image.";
+                }
+            }
         }
     }
 }
